686a8e5c93cf5476e46c0c4044ce24890d1bb2a4
Doug Lauver <admin@clicknip.com>
2014-02-24 19:52:38 -0600

    Set yourcodehere values

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,29 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'ych-bedrock'
+set :repo_url, 'git@github.com:yourcodehere/bedrock.git'
+# DAL going to leave default temporary directory /tmp
+# set :tmp_dir, '/home/doug/tmp/capistrano'
 
 # Branch options
 # Prompts for the branch name (defaults to current branch)
-#ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Sets branch to current one
-#set :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+# set :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Hardcodes branch to always be master
 # This could be overridden in a stage config file
-set :branch, :master
+# set :branch, :master
 
-set :deploy_to, "/srv/www/#{fetch(:application)}"
+set :deploy_to, "/home/doug/public/yourcodehere.com/deploy/#{fetch(:application)}"
 
-set :log_level, :info
+# Default is debug
+# set :log_level, :info
 
 set :linked_files, %w{.env}
 set :linked_dirs, %w{app/uploads}
 
 namespace :deploy do
-
+  # DAL I don't think it's actually required
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
